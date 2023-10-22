5586ca7a58ebe639298ba1778cbb36cf4a0392b6
Neeharika Baral <neehabaral72@gmail.com>
2023-10-22 01:49:48 -0400

    initial commit

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.2.32',
+    port: 50541
+  });
+
+  conn.on('connect', () => {
+    console.log("Connected to server");
+  });
+  
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting ...');
+
+connect();
+
